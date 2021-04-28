4db63325cae7c41b414e7caef87e35273b8dda4c
Shaked Lubin <shaked.lubin@gmail.com>
2021-04-28 19:35:35 +0300

    Beginning

--- src/Project1/project1.py
+++ src/Project1/project1.py
@@ -1,12 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-a, b = 2, 4
-
-
+#Part A
+a, b = 2, 8
 x, y = np.meshgrid(np.arange(-10, 10, 1), np.arange(-10, 10, 1))
-
 
 if (a + b) % 2 == 0:
     u, v = 0, (a+b)
@@ -18,5 +15,24 @@ else:
 fig, ax = plt.subplots()
 q = ax.quiver(x, y, u, v)
 plt.show()
+
+
+def elctricFieldInD(x,y):
+    if (x==0 and y==0):
+        pass
+    else:
+        if x==0:
+            theta = 90
+        else:
+            theta = np.arctan(y/x)
+            
+        baseVal = 144/(x**2+y**2)
+        xVal = baseVal*np.cos(theta)
+        yVal = baseVal*np.sin(theta)
+    
+    return (xVal, yVal)
+
+#z = (a+b)*y;
+#v,u = np.gradient(z)
 
 
