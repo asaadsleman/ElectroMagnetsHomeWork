328b8a50b1aadd11bedd368f20a5468989fc53bd
Shaked Lubin <shaked.lubin@gmail.com>
2021-05-03 18:08:35 +0300

    Small fixes

--- src/Project1/project1.py
+++ src/Project1/project1.py
@@ -2,45 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #~~~~~Part A~~~~~
-def electricFieldPartAa():
-    a, b = 2, 8
+def electricFieldConstant():
     x, y = np.meshgrid(np.arange(-10, 10, 1), np.arange(-10, 10, 1))
-    u, v = 0, (a+b)
+    u, v = 0, 10
 
     fig, ax = plt.subplots()
     ax.quiver(x, y, u, v)
     plt.show()
     
 
-def elctricFieldPartAe(x,y):
-    theta = np.arctan2(y,x)
-    
-    try:
-        baseVal = 144/(x**2+y**2)
-    except ZeroDivisionError:
-        baseVal = 0 
-        
-    xVal = baseVal*np.cos(theta)
-    yVal = baseVal*np.sin(theta)
-    
-    return (xVal, yVal)
-
-
-def elctricFieldPartAeQuiver():
-    x, y = np.meshgrid(np.arange(-1, 1, 0.08), np.arange(-1, 1, 0.08))
-    u,v = elctricFieldPartAe(x,y)
-    fig, ax = plt.subplots()
-    ax.quiver(x, y, u, v)
-    
-    pot = 144/((x**2+y**2)**0.5)
-    ax.contour(x,y,pot)
-    
-    plt.show()
-
-
-def elctricFieldPartAeDisplaced(z, w):
-    x = z - 2
-    y = w - 8
+def elctricFieldOfPointCharge(x, y, a=0, b=0):
+    x = x - a
+    y = y - b
     theta = np.arctan2(y, x)
 
     try:
@@ -51,31 +24,56 @@ def elctricFieldPartAeDisplaced(z, w):
     xVal = baseVal * np.cos(theta)
     yVal = baseVal * np.sin(theta)
 
-    return (xVal, yVal)
+    return [xVal, yVal]
 
 
-def elctricFieldPartAeQuiverDisplaced():
-    x, y = np.meshgrid(np.arange(1, 3, 0.08), np.arange(7, 9, 0.08))
-    u, v = elctricFieldPartAeDisplaced(x, y)
+def electricFieldQuiverForPointCharge(a=0, b=0):
+    x, y = np.meshgrid(np.arange(a-1, a+1, 0.08), np.arange(b-1, b+1, 0.08))
+    u, v = elctricFieldOfPointCharge(x, y, a, b)
     fig, ax = plt.subplots()
     ax.quiver(x, y, u, v)
 
-    pot = 144 / (((x-2) ** 2 + (y-8) ** 2) ** 0.5)
+    pot = 144 / (((x-a) ** 2 + (y-b) ** 2) ** 0.5)
     ax.contour(x, y, pot)
 
     plt.show()
 
 
-electricFieldPartAa()
-elctricFieldPartAeQuiver()
-elctricFieldPartAeQuiverDisplaced()
-
 
 
 #~~~~~Part B~~~~~
-def ElectricFieldQuiverB():
-    x, y = np.meshgrid(np.arange(-10, 10, .2), np.arange(-10, 10, .2))
-    r = ((x ** 2 + y ** 2)**0.5)
+def electricFieldOfDipole(x, y, a1, b1, a2, b2):
+    v1 = elctricFieldOfPointCharge(x,y,a1,b1)
+    v2 = elctricFieldOfPointCharge(x,y,a2,b2)
+    v3 = [[0 for i in range(len(v1[0]))] for j in range(len(v1))]
+    
+    for i in range(len(v1)):
+        for j in range(len(v1[i])):
+            v3[i][j] = v1[i][j]-v2[i][j]
+
+    return v3
+
+    
+    
+def ElectricFieldQuiverForDipole():
+    d = 2*pow(10,-6)
+    jump = d/8
+    
+    x, y = np.meshgrid(np.arange(-2*d, 2*d, jump), np.arange(-2*d, 2*d, jump))
+    u, v = electricFieldOfDipole(x, y, 0, -d/2, 0, d/2)
     fig, ax = plt.subplots()
-    q = ax.quiver(x, y, x/(r**3), y/(r**3))
+    ax.quiver(x, y, u, v,scale=pow(10,16))
     plt.show()
+
+
+
+def runPartA():
+    electricFieldConstant()
+    electricFieldQuiverForPointCharge()
+    electricFieldQuiverForPointCharge(2,8)
+
+def runPartB():
+    ElectricFieldQuiverForDipole()
+    
+runPartA()
+runPartB()
