20b60644964e83492e50d5f959e9f202117f969b
Shaked Lubin <shaked.lubin@gmail.com>
2021-05-03 16:16:53 +0300

    More to part A

--- src/Project1/project1.py
+++ src/Project1/project1.py
@@ -1,45 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Part A
-a, b = 2, 8
-x, y = np.meshgrid(np.arange(-10, 10, 1), np.arange(-10, 10, 1))
-
-if (a + b) % 2 == 0:
-    u, v = 0, (a+b)
-else:
-    u = (a*b*x)/(pow(x, 2) + pow(y, 2))
-    v = (a*b*y)/(pow(x, 2) + pow(y, 2))
-
-
-fig, ax = plt.subplots()
-q = ax.quiver(x, y, u, v)
-plt.show()
-
-
-def elctricFieldInD(x,y):
-    if (x==0 and y==0):
-        return (0, 0)
+#~~~~~Part A~~~~~
+def electricFieldPartAa():
+    a, b = 2, 8
+    x, y = np.meshgrid(np.arange(-10, 10, 1), np.arange(-10, 10, 1))
+    
+    if (a + b) % 2 == 0:
+        u, v = 0, (a+b)
     else:
-        if x==0:
-            theta = 90
-        else:
-            theta = np.arctan(y/x)
-            
+        u = (a*b*x)/(pow(x, 2) + pow(y, 2))
+        v = (a*b*y)/(pow(x, 2) + pow(y, 2))
+    
+    
+    fig, ax = plt.subplots()
+    q = ax.quiver(x, y, u, v)
+    plt.show()
+    
+
+def elctricFieldPartAe(x,y):
+    theta = np.arctan2(y,x)
+    
+    try:
         baseVal = 144/(x**2+y**2)
-        xVal = baseVal*np.cos(theta)
-        yVal = baseVal*np.sin(theta)
+    except ZeroDivisionError:
+        baseVal = 0 
+        
+    xVal = baseVal*np.cos(theta)
+    yVal = baseVal*np.sin(theta)
     
     return (xVal, yVal)
 
-#z = (a+b)*y;
-#v,u = np.gradient(z)
 
+def elctricFieldPartAeQuiver():
+    x, y = np.meshgrid(np.arange(-1, 1, 0.08), np.arange(-1, 1, 0.08))
+    u,v = elctricFieldPartAe(x,y)
+    fig, ax = plt.subplots()
+    ax.quiver(x, y, u, v)
+    
+    pot = 144/((x**2+y**2)**0.5)
+    ax.contour(x,y,pot)
+    
+    plt.show()
+
+
+#electricFieldPartAa()
+elctricFieldPartAeQuiver()
+#elctricFieldPartAHeQuiver()
+
+
+#~~~~~Part B~~~~~
 def ElectricFieldQuiverB():
     x, y = np.meshgrid(np.arange(-10, 10, .2), np.arange(-10, 10, .2))
     r = ((x ** 2 + y ** 2)**0.5)
     fig, ax = plt.subplots()
     q = ax.quiver(x, y, x/(r**3), y/(r**3))
     plt.show()
-
-#Part B
